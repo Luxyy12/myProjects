1b7742d8cb267c39eaf91a23ceec0e907f1bddee
Luxyy12 <lsauerwork@gmail.com>
2022-11-19 12:42:17 +0100

    Even and Odd Numbers

--- EvenAndOddNumbers/src/Main.java
+++ EvenAndOddNumbers/src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-      /*  int[] numbers = new int[] {
+        int[] numbers = new int[] {
                 1, 5, 10, 13, 25, 50, 100, 125, 133, 152, 200
-        };*/
+        };
         String oddNumbers = "";
         String evenNumbers = "";
 
-     /*   for(int i = 0; i < numbers.length; i++){
+        for(int i = 0; i < numbers.length; i++){
             if(numbers[i] % 2 == 0)
-                evenNumbers += String.valueOf(numbers[i]);
+                evenNumbers += String.valueOf(numbers[i]) + " ";
             else
-                oddNumbers += String.valueOf(numbers[i]);
+                oddNumbers += String.valueOf(numbers[i]) + " ";
         }
-*/
+
         System.out.println("even numbers: " + evenNumbers);
         System.out.println("odd numbers: " + oddNumbers);
     }
